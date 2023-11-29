d0711cf1d5144ab9f569bc9f77291af9e6319906
Kristian Antonsen <kristian@derfor.dk>
2023-11-29 18:10:27 +0100

    Fix ingest token test (#8)

--- examples/ingest_token.tf
+++ examples/ingest_token.tf
@@ -1,18 +1,24 @@
 resource "humio_ingest_token" "example_ingest_token_without_parser" {
-  repository = "humio"
+  repository = "sandbox"
   name       = "example_ingest_token_without_parser"
 }
 
 resource "humio_ingest_token" "example_ingest_token_with_accesslog_parser" {
-  repository = "humio"
+  repository = "sandbox"
   name       = "example_ingest_token_with_accesslog_parser"
   parser     = "accesslog"
+
+  depends_on = [
+    humio_ingest_token.example_ingest_token_without_parser
+  ]
 }
 
 output "ingest_token_without_parser" {
-  value = humio_ingest_token.example_ingest_token_without_parser.token
+  value     = humio_ingest_token.example_ingest_token_without_parser.token
+  sensitive = true
 }
 
 output "ingest_token_with_accesslog_parser" {
-  value = humio_ingest_token.example_ingest_token_with_accesslog_parser.token
+  value     = humio_ingest_token.example_ingest_token_with_accesslog_parser.token
+  sensitive = true
 }
